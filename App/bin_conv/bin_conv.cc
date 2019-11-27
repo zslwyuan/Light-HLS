8f756e20fa0726c6f50218fad71bf7339274b6b4
Tingyuan <tliang@connect.ust.hk>
2019-11-28 02:08:39 +0800

    TODO: disable sroa

--- App/bin_conv/bin_conv.cc
+++ App/bin_conv/bin_conv.cc
@@ -125,11 +125,15 @@ void process_word(const TwoBit word_buffer_m[CONV_BANKS][CONV_COLS], const TwoBi
 // output feature map.
 // * Make sure this function gets inlined by VHLS, or cosim may fail!
 // -----------------------------------------------------------------------
-void bin_conv(Word wt_mem[CONVOLVERS][C_WT_WORDS], NormComp nc, Word dmem[2][CONVOLVERS][C_DMEM_WORDS], ap_uint<1> d_i_idx, ap_uint<1> d_o_idx, const unsigned n_inputs, const Address o_index, const ap_uint<1> new_batch,
-              const ap_uint<2> width_mode, // 0=8'b, 1=16'b, 2=32'b
-              const ap_uint<2> norm_mode   // 0='do nothing', 1='do norm', 2='do pool'
-)
+// void bin_conv(Word wt_mem[CONVOLVERS][C_WT_WORDS], NormComp nc, Word dmem[2][CONVOLVERS][C_DMEM_WORDS], ap_uint<1> d_i_idx, ap_uint<1> d_o_idx, const unsigned n_inputs, const Address o_index, const ap_uint<1> new_batch,
+//               const ap_uint<2> width_mode, // 0=8'b, 1=16'b, 2=32'b
+//               const ap_uint<2> norm_mode   // 0='do nothing', 1='do norm', 2='do pool'
+// )
+// {
+void bin_conv(Word wt_mem[CONVOLVERS][C_WT_WORDS], NormComp nc, Word dmem[2][CONVOLVERS][C_DMEM_WORDS], ap_uint<1> d_i_idx, ap_uint<1> d_o_idx, const unsigned n_inputs, const Address o_index, const ap_uint<1> new_batch)
 {
+              const ap_uint<2> width_mode = 0; // 0=8'b, 1=16'b, 2=32'b
+              const ap_uint<2> norm_mode = 0;  // 0='do nothing', 1='do norm', 2='do pool'
     const ap_uint<3> log_width = width_mode + LOG_BANK_WIDTH;
     const ap_uint<5> words_per_image = 1 << (2 * width_mode);
     const unsigned n_phases = n_inputs / CONVOLVERS;
